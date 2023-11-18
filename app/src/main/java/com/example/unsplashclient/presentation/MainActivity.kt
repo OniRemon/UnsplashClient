69bfb7d8f19f6b4e43c4d9f08e8fda3773a7ccbd
OniRemon <saito4431@gmail.com>
2023-11-18 17:34:48 +0900

    画像検索画面のViewベース作成

--- app/src/main/java/com/example/unsplashclient/presentation/MainActivity.kt
+++ app/src/main/java/com/example/unsplashclient/presentation/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.unsplashclient.presentation.search_photos.SearchPhotosScreen
 import com.example.unsplashclient.presentation.ui.theme.UnsplashClientTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +33,7 @@ class MainActivity : ComponentActivity() {
                         //画像検索画面
                         composable(route = ScreenRoute.SearchPhotosScreen.route) {
                             //TODO
-                            Text(text = "画像検索画面")
+                            SearchPhotosScreen()
                         }
                         //画像詳細表示画面
                         composable(route = ScreenRoute.PhotoDetailScreen.route) {
