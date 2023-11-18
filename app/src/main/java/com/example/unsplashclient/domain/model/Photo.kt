d4c2c3b1fb5f9d39134f3a0bc7af91bae0bb8003
OniRemon <saito4431@gmail.com>
2023-11-18 15:01:31 +0900

    Photoモデル、PhotoDetailモデル作成

---
+++ app/src/main/java/com/example/unsplashclient/domain/model/Photo.kt
@@ -0,0 +1,10 @@
+package com.example.unsplashclient.domain.model
+
+data class Photo(
+    val photoId: String,
+    val description: String?,
+    val likes: Int?,
+    val imageUrl: String,
+    val photographer: String?,
+
+)
