18a2dd494e6d4877d3987e43f89748a4743a1905
OniRemon <saito4431@gmail.com>
2023-12-02 18:16:50 +0900

    PhotoDetailContent作成

--- app/src/main/java/com/example/unsplashclient/presentation/photo_detail/PhotoDetailScreen.kt
+++ app/src/main/java/com/example/unsplashclient/presentation/photo_detail/PhotoDetailScreen.kt
@@ -1,16 +1,29 @@
 package com.example.unsplashclient.presentation.photo_detail
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
+import com.example.unsplashclient.domain.model.PhotoDetail
 
 @Composable
 fun PhotoDetailScreen(
@@ -40,6 +53,35 @@ fun PhotoDetailScreen(
                     Text(text = "通信成功")
                 }
             }
+        }
+    }
+}
+
+
+@Composable
+fun PhotoDetailContent(photoDetail: PhotoDetail) {
+    // スクロールできるようにする
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+        Box(modifier = Modifier.heightIn(200.dp)) {
+            var isLoadingImage  by remember { mutableStateOf(true) }
+            if (isLoadingImage) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            }
+            AsyncImage(
+                model = photoDetail.imageUrl,
+                contentDescription = photoDetail.description,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(
+                        RoundedCornerShape(
+                            topStartPercent = 0,
+                            topEndPercent = 0,
+                            bottomStartPercent = 5,
+                            bottomEndPercent = 5
+                        )
+                    ),
+                onSuccess = { isLoadingImage = false },
+            )
         }
     }
 }
